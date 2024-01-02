1a355e33e8a52ebb8315b9896d6361d00c67b12f
Ji-Xinyou <jerryji0414@outlook.com>
2024-01-02 21:10:24 +0800

    fix(nvim): db

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,4 +3,12 @@ return {
         'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    {
+        "ahmedkhalf/project.nvim",
+        config = function()
+            require("project_nvim").setup {}
+            require('telescope').load_extension('projects')
+        end
+    },
 }
+
